547c9db82a551522ed6c61e679629034d5916725
Ryan Cotter <ryancotter@gitlab.com>
2016-11-29 21:51:12 +0800

    Added web pages

---
+++ web pages/Requests: HTTP for Humans — Requests 2.12.1 documentation_files/readthedocs-data.js
@@ -0,0 +1,14 @@
+  var READTHEDOCS_DATA = {
+    project: "requests",
+    version: "master",
+    language: "en",
+    subprojects: {},
+    canonical_url: "http://docs.python-requests.org/en/master/",
+    theme: "alabaster",
+    builder: "sphinx",
+    docroot: "/docs/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "5c454949"
+  };
+  
